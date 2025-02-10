2692ee07180c155aee944c45ec5fbac133cfbb91
Olushola <ememobongumoren2@gmail.com>
2025-02-10 16:03:01 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "orange": "hsl(25, 97%, 53%)",
+        "white": "hsl(0, 0%, 100%)",
+        "lightGrey": "hsl(217, 12%, 63%)",
+        "darkBlue": "hsl(213, 19%, 18%)",
+        "veryDarkBlue": "hsl(216, 12%, 8%)",
       },
     },
   },
